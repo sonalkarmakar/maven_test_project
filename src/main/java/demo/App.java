39aadbce3f37eaaf436d2b0980b0e73ca2bd6f76
Sonal Karmakar <234934724+sonalkarmakar@users.noreply.github.com>
2025-11-27 15:46:39 +0530

    Updated App.java
    Updated App.java to have a cleaner formatting.

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -1,13 +1,7 @@
 package demo;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World! from CI CD task package hi from demo by jenkins" );
-    }
+public class App {
+	public static void main( String[] args ) {
+		System.out.println( "Hello World!\nHello from CI/CD task package.\nHi from demo by Jenkins!" );
+	}
 }
