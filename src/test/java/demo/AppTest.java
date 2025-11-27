0492ed929855a71d3ac3079bbe0bca183094c557
Sonal Karmakar <234934724+sonalkarmakar@users.noreply.github.com>
2025-11-27 15:48:33 +0530

    Updated AppTest.java
    Updated AppTest.java to have better formatting.

--- src/test/java/demo/AppTest.java
+++ src/test/java/demo/AppTest.java
@@ -1,20 +1,13 @@
 package demo;
 
 import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+//Unit test for simple App.
+public class AppTest {
+	// Rigorous Test :-)
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+	public void shouldAnswerWithTrue() {
+		assertTrue( true );
+	}
 }
